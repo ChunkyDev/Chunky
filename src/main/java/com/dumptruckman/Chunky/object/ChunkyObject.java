fdc01ab9f671e6204707584cb845714b617093dc
SwearWord <swearword@blockface.org>
2011-08-29 15:38:11 -0400

    Removed serializable

--- src/main/java/com/dumptruckman/Chunky/object/ChunkyObject.java
+++ src/main/java/com/dumptruckman/Chunky/object/ChunkyObject.java
@@ -6,7 +6,7 @@ import java.util.Observable;
 /**
  * @author dumptruckman
  */
-public class ChunkyObject extends Observable implements Serializable {
+public class ChunkyObject extends Observable {
 
     private String name;
 
