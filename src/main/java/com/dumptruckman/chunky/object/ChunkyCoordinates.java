baaca9e67c4b25fd60b677eee06ee99738d35deb
Jeremy Wood <farachan@gmail.com>
2011-08-29 23:03:42 -0700

    Added World to Coords

--- src/main/java/com/dumptruckman/chunky/object/ChunkyCoordinates.java
+++ src/main/java/com/dumptruckman/chunky/object/ChunkyCoordinates.java
@@ -3,39 +3,50 @@ package com.dumptruckman.chunky.object;
 
 import org.bukkit.Location;
 
+/**
+ * @author dumptruckman, SwearWord
+ */
 public class ChunkyCoordinates {
-    private int X;
-    private int Z;
+    
+    private int x;
+    private int z;
+    private String world;
 
     public ChunkyCoordinates(Location location)
     {
-        this.X = (int)(location.getX()/16)-1;
-        this.Z = (int)(location.getZ()/16)-1;
+        this.x = (int)(location.getX()/16)-1;
+        this.z = (int)(location.getZ()/16)-1;
+        this.world = location.getWorld().getName();
     }
 
-    public ChunkyCoordinates(double x, double z)
+    public ChunkyCoordinates(String world, double x, double z)
     {
-        this.X = (int)(x/16)-1;
-        this.Z = (int)(z/16)-1;
+        this.world = world;
+        this.x = (int)(x/16)-1;
+        this.z = (int)(z/16)-1;
     }
 
     public int getX() {
-        return this.X;
+        return this.x;
     }
 
     public int getZ() {
-        return this.Z;
+        return this.z;
+    }
+
+    public String getWorld() {
+        return this.world;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(!(obj instanceof ChunkyCoordinates)) return false;
+        if (!(obj instanceof ChunkyCoordinates)) return false;
         ChunkyCoordinates coords = (ChunkyCoordinates)obj;
-        return coords.getX() == this.X && coords.getZ() == this.Z;
+        return coords.getWorld().equals(this.getWorld()) && coords.getX() == this.x && coords.getZ() == this.z;
     }
 
     @Override
     public int hashCode() {
-        return (this.getX() + "," + this.getZ()).hashCode();
+        return (this.getWorld() + "," + this.getX() + "," + this.getZ()).hashCode();
     }
 }
