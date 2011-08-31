afc4d73b0dbc326857bcda45758557c73b4723e0
Jeremy Wood <farachan@gmail.com>
2011-08-31 14:03:03 -0400

    Removed some of the old ownership stuff from player. x2

--- src/main/java/com/dumptruckman/chunky/object/ChunkyPlayer.java
+++ src/main/java/com/dumptruckman/chunky/object/ChunkyPlayer.java
@@ -4,19 +4,15 @@ import com.dumptruckman.chunky.exceptions.ChunkyPlayerOfflineException;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.HashSet;
-
 /**
  * @author dumptruckman, SwearWord
  */
 public class ChunkyPlayer extends ChunkyObject {
 
     private ChunkyChunk lastChunk;
-    private HashSet<ChunkyChunk> chunks;
 
     public ChunkyPlayer(String name) {
         super(name);
-        chunks = new HashSet<ChunkyChunk>();
     }
 
     public void setLastChunk(ChunkyChunk chunk) {
