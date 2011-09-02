abc2b7557ea16e404791120fe5fd21a94a40513b
SwearWord <swearword@blockface.org>
2011-09-01 22:15:55 -0400

    Switched the Switch event. Puns!

--- src/main/java/com/dumptruckman/chunky/event/object/player/ChunkyPlayerSwitchEvent.java
+++ src/main/java/com/dumptruckman/chunky/event/object/player/ChunkyPlayerSwitchEvent.java
@@ -2,6 +2,7 @@ package com.dumptruckman.chunky.event.object.player;
 
 import com.dumptruckman.chunky.object.ChunkyChunk;
 import com.dumptruckman.chunky.object.ChunkyPlayer;
+import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.inventory.ItemStack;
 
@@ -10,16 +11,16 @@ import org.bukkit.inventory.ItemStack;
  */
 public class ChunkyPlayerSwitchEvent extends ChunkyPlayerChunkEvent implements Cancellable {
 
-    private ItemStack item;
+    private Block switchedBlock;
     private boolean cancel = false;
 
-    public ChunkyPlayerSwitchEvent(ChunkyPlayer chunkyPlayer, ChunkyChunk chunkyChunk, ItemStack item) {
+    public ChunkyPlayerSwitchEvent(ChunkyPlayer chunkyPlayer, ChunkyChunk chunkyChunk, Block switchedBlock) {
         super(Type.PLAYER_SWITCH, chunkyPlayer, chunkyChunk);
-        this.item = item;
+        this.switchedBlock = switchedBlock;
     }
 
-    public final ItemStack getItem() {
-        return item;
+    public final Block getSwitchedBlock() {
+        return this.switchedBlock;
     }
 
     public boolean isCancelled() {
