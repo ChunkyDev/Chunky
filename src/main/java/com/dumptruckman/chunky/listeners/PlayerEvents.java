f2ec89cafd014f20daf37e9f92e99133bbf791b1
SwearWord <swearword@blockface.org>
2011-09-01 21:45:01 -0400

    Added event firing for ChunkPlayeritemUseEvent

--- src/main/java/com/dumptruckman/chunky/listeners/PlayerEvents.java
+++ src/main/java/com/dumptruckman/chunky/listeners/PlayerEvents.java
@@ -3,14 +3,15 @@ package com.dumptruckman.chunky.listeners;
 import com.dumptruckman.chunky.Chunky;
 import com.dumptruckman.chunky.ChunkyManager;
 import com.dumptruckman.chunky.event.object.player.ChunkyPlayerChunkChangeEvent;
+import com.dumptruckman.chunky.event.object.player.ChunkyPlayerItemUseEvent;
 import com.dumptruckman.chunky.locale.Language;
+import com.dumptruckman.chunky.module.ChunkyModuleManager;
+import com.dumptruckman.chunky.module.SimpleChunkyModuleManager;
 import com.dumptruckman.chunky.object.ChunkyChunk;
 import com.dumptruckman.chunky.object.ChunkyPlayer;
 import com.dumptruckman.chunky.util.Logging;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.*;
 
 /**
  * @author dumptruckman, SwearWord
@@ -51,5 +52,17 @@ public class PlayerEvents extends PlayerListener{
         if (event.isCancelled()) return;
         String[] commands = event.getMessage().substring(1).split("\\s");
         Chunky.getModuleManager().parseCommand(event.getPlayer(), commands);
+    }
+
+    @Override
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && !event.isBlockInHand()) {
+            ChunkyPlayer chunkyPlayer = ChunkyManager.getChunkyPlayer(event.getPlayer().getName());
+            ChunkyChunk chunkyChunk = ChunkyManager.getChunk(event.getClickedBlock().getLocation());
+            ChunkyPlayerItemUseEvent chunkyEvent = new ChunkyPlayerItemUseEvent(chunkyPlayer,chunkyChunk,event.getItem());
+            Chunky.getModuleManager().callEvent(chunkyEvent);
+            event.setCancelled(chunkyEvent.isCancelled());
+        }
+
     }
 }
