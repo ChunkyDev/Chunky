3c2d984fb18c637f43b07fabde5d0bf18c4c6b32
Jeremy Wood <farachan@gmail.com>
2011-09-03 21:03:51 -0700

    ChunkyOwnershipNode with all empty implemented methods

--- src/main/java/com/dumptruckman/chunky/object/ChunkyOwnershipNode.java
+++ src/main/java/com/dumptruckman/chunky/object/ChunkyOwnershipNode.java
@@ -7,5 +7,47 @@ import javax.swing.tree.MutableTreeNode;
  */
 public class ChunkyOwnershipNode implements MutableTreeNode {
 
+    /**
+     * Adds <code>child</code> to the receiver at <code>index</code>.
+     * <code>child</code> will be messaged with <code>setParent</code>.
+     */
+    public void insert(MutableTreeNode child, int index) {
 
+    }
+
+    /**
+     * Removes the child at <code>index</code> from the receiver.
+     */
+    public void remove(int index) {
+
+    }
+
+    /**
+     * Removes <code>node</code> from the receiver. <code>setParent</code>
+     * will be messaged on <code>node</code>.
+     */
+    public void remove(MutableTreeNode node) {
+
+    }
+
+    /**
+     * Resets the user object of the receiver to <code>object</code>.
+     */
+    public void setUserObject(Object object) {
+
+    }
+
+    /**
+     * Removes the receiver from its parent.
+     */
+    public void removeFromParent() {
+
+    }
+
+    /**
+     * Sets the parent of the receiver to <code>newParent</code>.
+     */
+    public void setParent(MutableTreeNode newParent) {
+
+    }
 }
