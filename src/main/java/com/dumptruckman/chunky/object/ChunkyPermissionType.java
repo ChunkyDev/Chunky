8b407006923d34e5b20f30f879a69884402b1010
Jeremy Wood <farachan@gmail.com>
2011-09-05 23:03:59 -0700

    Added ChunkyPermissionType to indicate what kind of permission were used in events.

---
+++ src/main/java/com/dumptruckman/chunky/object/ChunkyPermissionType.java
@@ -0,0 +1,11 @@
+package com.dumptruckman.chunky.object;
+
+/**
+ * @author dumptruckman, SwearWord
+ */
+public enum ChunkyPermissionType {
+    OWNER,
+    DIRECT_OWNER,
+    PERMISSION,
+    NONE,
+}
