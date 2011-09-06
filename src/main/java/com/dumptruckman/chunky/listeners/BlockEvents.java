b9b20b69b2ab6851f171729beebe59fb081ae62f
Jeremy Wood <farachan@gmail.com>
2011-09-05 23:04:43 -0700

    Fixed BlockEvents to work on the new permissions.

--- src/main/java/com/dumptruckman/chunky/listeners/BlockEvents.java
+++ src/main/java/com/dumptruckman/chunky/listeners/BlockEvents.java
@@ -5,22 +5,38 @@ import com.dumptruckman.chunky.ChunkyManager;
 import com.dumptruckman.chunky.event.object.player.ChunkyPlayerUnownedBreakEvent;
 import com.dumptruckman.chunky.event.object.player.ChunkyPlayerUnownedBuildEvent;
 import com.dumptruckman.chunky.object.ChunkyChunk;
+import com.dumptruckman.chunky.object.ChunkyPermissionType;
+import com.dumptruckman.chunky.object.ChunkyPermissions;
 import com.dumptruckman.chunky.object.ChunkyPlayer;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockEvents extends BlockListener {
+
     @Override
     public void onBlockPlace(BlockPlaceEvent event) {
         ChunkyPlayer chunkyPlayer = ChunkyManager.getChunkyPlayer(event.getPlayer().getName());
         ChunkyChunk chunk = ChunkyManager.getChunk(event.getBlock().getLocation());
-        if(!chunk.isOwnedBy(chunkyPlayer)) onUnownedChunkBuild(event, chunkyPlayer, chunk);
-    }
 
-    public void onUnownedChunkBuild(BlockPlaceEvent event, ChunkyPlayer builder, ChunkyChunk chunkyChunk) {
-        ChunkyPlayerUnownedBuildEvent chunkyEvent = new ChunkyPlayerUnownedBuildEvent(builder, chunkyChunk, event.getBlock());
+        boolean isCancelled = true;
+        ChunkyPermissionType permType = ChunkyPermissionType.NONE;
+
+        // Permission chain
+        if (chunk.isOwnedBy(chunkyPlayer)) {
+            permType = ChunkyPermissionType.OWNER;
+            if (chunk.isDirectlyOwnnedBy(chunkyPlayer)) permType = ChunkyPermissionType.DIRECT_OWNER;
+            isCancelled = false;
+        } else if (chunkyPlayer.hasPerm(chunk, ChunkyPermissions.Flags.BUILD)) {
+            permType = ChunkyPermissionType.PERMISSION;
+            isCancelled = false;
+        }
+        //Groups here?:
+
+        ChunkyPlayerUnownedBuildEvent chunkyEvent = new ChunkyPlayerUnownedBuildEvent(chunkyPlayer, chunk, event.getBlock(), permType);
+        chunkyEvent.setCancelled(isCancelled);
         Chunky.getModuleManager().callEvent(chunkyEvent);
+
         event.setCancelled(chunkyEvent.isCancelled());
     }
 
@@ -28,13 +44,25 @@ public class BlockEvents extends BlockListener {
     public void onBlockBreak(BlockBreakEvent event) {
         ChunkyPlayer chunkyPlayer = ChunkyManager.getChunkyPlayer(event.getPlayer().getName());
         ChunkyChunk chunk  = ChunkyManager.getChunk(event.getBlock().getLocation());
-        if(!chunk.isOwnedBy(chunkyPlayer)) onUnownedChunkBreak(event, chunkyPlayer, chunk);
-    }
 
-    public void onUnownedChunkBreak(BlockBreakEvent event, ChunkyPlayer breaker, ChunkyChunk chunkyChunk) {
-        event.setCancelled(true);
-        ChunkyPlayerUnownedBreakEvent chunkyEvent = new ChunkyPlayerUnownedBreakEvent(breaker,chunkyChunk,event.getBlock());
+        boolean isCancelled = true;
+        ChunkyPermissionType permType = ChunkyPermissionType.NONE;
+
+        // Permission chain
+        if (chunk.isOwnedBy(chunkyPlayer)) {
+            permType = ChunkyPermissionType.OWNER;
+            if (chunk.isDirectlyOwnnedBy(chunkyPlayer)) permType = ChunkyPermissionType.DIRECT_OWNER;
+            isCancelled = false;
+        } else if (chunkyPlayer.hasPerm(chunk, ChunkyPermissions.Flags.DESTROY)) {
+            permType = ChunkyPermissionType.PERMISSION;
+            isCancelled = false;
+        }
+        //Groups here?:
+
+        ChunkyPlayerUnownedBreakEvent chunkyEvent = new ChunkyPlayerUnownedBreakEvent(chunkyPlayer, chunk, event.getBlock(), permType);
+        chunkyEvent.setCancelled(isCancelled);
         Chunky.getModuleManager().callEvent(chunkyEvent);
+        
         event.setCancelled(chunkyEvent.isCancelled());
     }
 }
