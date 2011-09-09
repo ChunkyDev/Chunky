aef609275ed880726db808121ba62239daca8637
Jeremy Wood <farachan@gmail.com>
2011-09-09 11:36:47 -0400

    Further changes to /chunky permission.  Still needs feedback.

--- src/main/java/com/dumptruckman/chunky/command/CommandChunkyPermission.java
+++ src/main/java/com/dumptruckman/chunky/command/CommandChunkyPermission.java
@@ -8,10 +8,7 @@ import com.dumptruckman.chunky.exceptions.ChunkyException;
 import com.dumptruckman.chunky.locale.Language;
 import com.dumptruckman.chunky.module.ChunkyCommand;
 import com.dumptruckman.chunky.module.ChunkyCommandExecutor;
-import com.dumptruckman.chunky.object.ChunkyChunk;
-import com.dumptruckman.chunky.object.ChunkyObject;
-import com.dumptruckman.chunky.object.ChunkyPermissions;
-import com.dumptruckman.chunky.object.ChunkyPlayer;
+import com.dumptruckman.chunky.object.*;
 
 import com.dumptruckman.chunky.persistance.DatabaseManager;
 import org.bukkit.command.CommandSender;
@@ -24,14 +21,14 @@ public class CommandChunkyPermission implements ChunkyCommandExecutor {
 
     public void onCommand(CommandSender sender, ChunkyCommand command, String label, String[] args) {
         if (!(sender instanceof Player)){
-        	Language.IN_GAME_ONLY.bad(sender);
-        	return;
+            Language.IN_GAME_ONLY.bad(sender);
+            return;
         }
 
         if (args.length == 0){
             // TODO: Maybe this should show a readout of permissions given or something.
-        	Language.CMD_CHUNKY_PERMISSION_HELP.help(sender);
-        	return;
+            Language.CMD_CHUNKY_PERMISSION_HELP.help(sender);
+            return;
         }
         
         setPerms(ChunkyManager.getChunkyPlayer((Player) sender), args);
@@ -44,90 +41,89 @@ public class CommandChunkyPermission implements ChunkyCommandExecutor {
         ChunkyObject target = null;
         
         if (tokens.length == 1){
-        	if (!cPlayer.getCurrentChunk().isDirectlyOwnedBy(cPlayer)){
-        		Language.CHUNK_NOT_OWNED.bad(cPlayer);
-        		return;
-        	}
-        	target = cPlayer.getCurrentChunk();
-        	permissions = tokens[0];
+            if (!cPlayer.getCurrentChunk().isDirectlyOwnedBy(cPlayer)){
+                Language.CHUNK_NOT_OWNED.bad(cPlayer);
+                return;
+            }
+            target = cPlayer.getCurrentChunk();
+            permissions = tokens[0];
         } else if (tokens.length == 2){
-        	if (tokens[0].equals("*")){
-        		HashMap<Integer, HashSet<ChunkyObject>> ownables = cPlayer.getOwnables();
-        		if (!ownables.containsKey(ChunkyChunk.class.getName().hashCode())){
-        			Language.CHUNK_NONE_OWNED.bad(cPlayer);
-        			return;
-        		}
-        		target = cPlayer;
-        	} else {
-        		Language.FEATURE_NYI.bad(cPlayer);
-        		return;
-        	}
-        	permissions = tokens[1];
+            if (tokens[0].equals("*")){
+                HashMap<Integer, HashSet<ChunkyObject>> ownables = cPlayer.getOwnables();
+                if (!ownables.containsKey(ChunkyChunk.class.getName().hashCode())){
+                    Language.CHUNK_NONE_OWNED.bad(cPlayer);
+                    return;
+                }
+                target = cPlayer;
+            } else {
+                Language.FEATURE_NYI.bad(cPlayer);
+                return;
+            }
+            permissions = tokens[1];
+        } else {
+            // WILL NEVER REACH THIS POINT
+            return;
         }
         
+        int state = 0;
+        if (permissions.startsWith("-")) state = -1;
+        if (permissions.startsWith("+")) state = 1;
+        EnumSet<ChunkyPermissions.Flags> flags = EnumSet.noneOf(ChunkyPermissions.Flags.class);
+        for (char perm : permissions.toCharArray()) {
+            ChunkyPermissions.Flags flag = ChunkyPermissions.Flags.get(perm);
+            if (flag == null) continue;
+            flags.add(flag);
+        }
+
         // No player or group defined
         if (args.length == 1){
             // TODO fix when objects have their own flags.
-        	Language.ERROR.bad(cPlayer, "That feature does not exist yet.");
-    		return;
+            switch (state) {
+                case -1:
+                    for (ChunkyPermissions.Flags flag : flags) {
+                        target.setDefaultPerm(flag, false);
+                    }
+                    break;
+
+                case 1:
+                    for (ChunkyPermissions.Flags flag : flags) {
+                        target.setDefaultPerm(flag, true);
+                    }
+                    break;
+
+                case 0:
+                    target.setDefaultPerms(flags);
+                    break;
+            }
         } else if (args.length == 2) {
-        	if (args[1].startsWith("g:")){
-        		Language.FEATURE_NYI.bad(cPlayer);
-        		return;
-        	} else {
-                ChunkyPlayer permPlayer = ChunkyManager.getChunkyPlayer(args[1]);
-                int state = 0;
-                if (permissions.startsWith("-")) state = -1;
-                if (permissions.startsWith("+")) state = 1;
-                EnumSet<ChunkyPermissions.Flags> flags = EnumSet.noneOf(ChunkyPermissions.Flags.class);
-                for (char perm : permissions.toCharArray()) {
-                    ChunkyPermissions.Flags flag = ChunkyPermissions.Flags.get(perm);
-                    if (flag == null) continue;
-                    flags.add(flag);
-                }
-                switch (state) {
-                    case -1:
-                        for (ChunkyPermissions.Flags flag : flags) {
-                            permPlayer.setPerm(target, flag, false);
-                        }
-                        break;
+            ChunkyPermissibleObject object = null;
+            if (args[1].startsWith("g:")){
+                Language.FEATURE_NYI.bad(cPlayer);
+                return;
+            } else {
+                object = ChunkyManager.getChunkyPlayer(args[1]);
+            }
 
-                    case 1:
-                        for (ChunkyPermissions.Flags flag : flags) {
-                            permPlayer.setPerm(target, flag, true);
-                        }
-                        break;
+            switch (state) {
+                case -1:
+                    for (ChunkyPermissions.Flags flag : flags) {
+                        object.setPerm(target, flag, false);
+                    }
+                    break;
 
-                    case 0:
-                        EnumSet<ChunkyPermissions.Flags> notSet = EnumSet.complementOf(flags);
-                        for (ChunkyPermissions.Flags flag : flags) {
-                            permPlayer.setPerm(target, flag, true);
-                        }
-                        for (ChunkyPermissions.Flags flag : notSet) {
-                            permPlayer.setPerm(target, flag, false);
-                        }
-                        break;
-                }
-                // TODO NEEDS FEEDBACK
-        	}
+                case 1:
+                    for (ChunkyPermissions.Flags flag : flags) {
+                        object.setPerm(target, flag, true);
+                    }
+                    break;
+
+                case 0:
+                    object.setPerms(target, flags);
+                    break;
+            }
         } else if (args.length > 2){
-        	Language.CMD_CHUNKY_PERMISSION_HELP.bad(cPlayer);
-        	return;
+            Language.CMD_CHUNKY_PERMISSION_HELP.bad(cPlayer);
+            return;
         }
-        
-        
-    }
-    
-    private ChunkyPermissions.Flags stringToPerm(String flag){
-    	if(flag.equalsIgnoreCase("build"))
-    		return ChunkyPermissions.Flags.BUILD;
-    	else if (flag.equalsIgnoreCase("destroy"))
-    		return ChunkyPermissions.Flags.DESTROY;
-    	else if (flag.equalsIgnoreCase("itemuse"))
-    		return ChunkyPermissions.Flags.ITEMUSE;
-    	else if (flag.equalsIgnoreCase("switch"))
-    		return ChunkyPermissions.Flags.SWITCH;
-    	//TODO: Add exception?
-    	return null;
     }
 }
