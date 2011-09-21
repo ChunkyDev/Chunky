24cee1c8916a7b4284f598b41b60cb9756342e48
Jeremy Wood <farachan@gmail.com>
2011-09-21 14:29:31 -0400

    Added javadocs to MinecraftTools.java.

--- src/main/java/com/dumptruckman/chunky/util/MinecraftTools.java
+++ src/main/java/com/dumptruckman/chunky/util/MinecraftTools.java
@@ -7,14 +7,32 @@ import com.dumptruckman.chunky.config.Config;
  */
 public class MinecraftTools {
 
+    /**
+     * Converts seconds to ticks.
+     *
+     * @param seconds
+     * @return seconds * 20 (ticks)
+     */
     public static long convertSecondsToTicks(long seconds) {
         return seconds * 20;
     }
 
+    /**
+     * Determines if an item is a switchable item
+     * 
+     * @param id Item id
+     * @return true if item can be switched
+     */
     public static Boolean isSwitchable(int id) {
         return Config.getSwitchables().contains(Integer.toString(id));
     }
 
+    /**
+     * Determines if an item is usable
+     *
+     * @param id Item id
+     * @return true if item can be switched
+     */
     public static Boolean isUsable(int id) {
         return Config.getUsables().contains(Integer.toString(id));
     }
