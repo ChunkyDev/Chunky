cfd867becd291fd78a20e0e69bb63576588f4338
Jeremy Wood <farachan@gmail.com>
2011-09-21 14:15:28 -0400

    Removed unhash methods

--- src/main/java/com/dumptruckman/chunky/ChunkyManager.java
+++ src/main/java/com/dumptruckman/chunky/ChunkyManager.java
@@ -167,38 +167,4 @@ public class ChunkyManager {
         }
         return permissions.get(object);
     }
-
-    /**
-    \* Returns a string with a hash equal to the argument.
-    \* @return string with a hash equal to the argument.
-    \*/
-    private static String unhashChunkyObject(int target) {
-        StringBuilder answer = new StringBuilder();
-        if (target < 0) {
-            // String with hash of Integer.MIN_VALUE, 0x80000000
-            answer.append("\\u0915\\u0009\\u001e\\u000c\\u0002");
-
-            if (target == Integer.MIN_VALUE)
-                return answer.toString();
-            // Find target without sign bit set
-            target = target & Integer.MAX_VALUE;
-        }
-
-        unhash0(answer, target);
-        return answer.toString();
-    }
-
-    private static void unhash0(StringBuilder partial, int target) {
-        int div = target / 31;
-        int rem = target % 31;
-
-        if (div <= Character.MAX_VALUE) {
-            if (div != 0)
-                partial.append((char)div);
-            partial.append((char)rem);
-        } else {
-            unhash0(partial, div);
-            partial.append((char)rem);
-        }
-    }
 }
