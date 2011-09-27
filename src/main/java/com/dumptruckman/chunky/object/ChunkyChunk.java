7c014e0df633c02b296d2334313b9095ae71bcb3
Jeremy Wood <farachan@gmail.com>
2011-09-27 11:14:03 -0400

    Removed ChunkyPermissionsObject.

--- src/main/java/com/dumptruckman/chunky/object/ChunkyChunk.java
+++ src/main/java/com/dumptruckman/chunky/object/ChunkyChunk.java
@@ -3,7 +3,7 @@ package com.dumptruckman.chunky.object;
 /**
  * @author dumptruckman, SwearWord
  */
-public class ChunkyChunk extends ChunkyPermissionsObject {
+public class ChunkyChunk extends ChunkyObject {
 
     private ChunkyCoordinates coord;
 
