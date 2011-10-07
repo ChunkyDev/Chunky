86abbdaf342d18c49f75256cc29a819c86afe41e
Jeremy Wood <farachan@gmail.com>
2011-10-07 15:31:19 -0400

    Added CUSTOM AccessLevel for modules.

--- src/main/java/org/getchunky/chunky/permission/AccessLevel.java
+++ src/main/java/org/getchunky/chunky/permission/AccessLevel.java
@@ -13,65 +13,104 @@ public enum AccessLevel {
     /**
      * Admin access
      */
-    ADMIN,
+    ADMIN("Admin"),
 
     /**
      * Unowned/Wilderness areas
      */
-    UNOWNED,
+    UNOWNED("Unowned"),
 
     /**
      * An inherited owner of this object. Someone that owns the direct owner.
      */
-    OWNER,
+    OWNER("Owner"),
 
     /**
      * The direct owner of the object.  As in: object.getOwner()
      */
-    DIRECT_OWNER,
+    DIRECT_OWNER("Direct Owner"),
 
     /**
      * Permission was given for this object to a specific object.
      */
-    DIRECT_PERMISSION,
+    DIRECT_PERMISSION("Direct Permission"),
 
     /**
      * Permission was given for this object to a group
      */
-    DIRECT_GROUP_PERMISSION,
+    DIRECT_GROUP_PERMISSION("Direct Group Permission"),
 
     /**
      * Permission was given for this object's owners properties for a specific object.
      */
-    GLOBAL_PERMISSION,
+    GLOBAL_PERMISSION("Global Permission"),
 
     /**
      * Permission was given for this object's owners properties for a specific group.
      */
-    GLOBAL_GROUP_PERMISSION,
+    GLOBAL_GROUP_PERMISSION("Global Group Permission"),
 
     /**
      * The default permission for this specific object (aka, what perms anyone has on this specific object.)
      */
-    DIRECT_DEFAULT_PERMISSION,
+    DIRECT_DEFAULT_PERMISSION("Direct Default Permission"),
 
     /**
      * The default permission for all objects owned by this object's owner.
      */
-    GLOBAL_DEFAULT_PERMISSION,
+    GLOBAL_DEFAULT_PERMISSION("Global Default Permission"),
 
     /**
      * No permission given.
      */
-    NONE,;
+    NONE("None"),
+
+    /**
+     * Custom Access Level (Usable by modules)
+     */
+    CUSTOM("Custom") {
+        public void setName(String name) {
+            this.name = name;
+        }
+    },;
 
     private boolean denied = false;
+    private String name;
 
+    AccessLevel(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets whether this access level denied permission or not
+     *
+     * @param deny true to deny
+     */
     public void setDenied(boolean deny) {
         this.denied = deny;
     }
 
+    /**
+     * Checks whether this access level caused denial or not
+     * @return true if denied at this access level
+     */
     public boolean causedDenial() {
         return denied;
     }
+
+    /**
+     * Gets the name of the access level.  Mostly used for custom modules.
+     *
+     * @return name of access level
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the access level.  Only sets name for CUSTOM
+     *
+     * @param name new name for access level
+     */
+    public void setName(String name) { }
 }
